b1e901e9268961f23192d9aced51b484dd4812d4
KejieLyu <33347544+KejieLyu@users.noreply.github.com>
2018-01-18 14:28:22 +0800

    Update image_reader.py

--- image_reader.py
+++ image_reader.py
@@ -22,12 +22,66 @@ def image_rotate(img, lab):
     """
     random_angle = tf.random_uniform([], minval=-math.pi/6, maxval=math.pi/6, dtype=tf.float32)
     lab = tf.cast(lab, dtype=tf.float32)
-    img -= 255.
     lab -= 255.
     img = tf.contrib.image.rotate(img, random_angle, "BILINEAR")
     lab = tf.contrib.image.rotate(lab, random_angle, "NEAREST")
-    img += 255.
     lab += 255.
+    lab = tf.cast(lab, dtype=tf.uint8)
+    return img, lab
+
+
+def image_scale(img, lab):
+    """
+    Randomly scales the image and label.
+
+    Inputs:
+        img: img to scale.
+        lab: lab to scale.
+    Outputs:
+        img: img after scale.
+        lab: lab after scale.
+    """
+    # Expand in dimension 0 for resize
+    img = tf.expand_dims(img, dim=0)
+    lab = tf.expand_dims(lab, dim=0)
+    # Randomly rescale the image and label
+    scale = tf.random_uniform([1], minval=0.5, maxval=1.5, dtype=tf.float32)
+    h_new = tf.to_int32(tf.multiply(tf.to_float(image_size[0]), scale))
+    w_new = tf.to_int32(tf.multiply(tf.to_float(image_size[1]), scale))
+    new_shape = tf.squeeze(tf.stack([h_new, w_new]), squeeze_dims=[1])
+    img = tf.image.resize_bilinear(img, new_shape)
+    lab = tf.image.resize_nearest_neighbor(lab, new_shape)
+    # Remove dimension 0
+    img = tf.squeeze(img, axis=0)
+    lab = tf.squeeze(lab, axis=0)
+    return img, lab
+
+
+def image_crop(img, lab):
+    """
+    Randomly crops the image and label.
+
+    Inputs:
+        img: img to crop.
+        lab: lab to crop.
+    Outputs:
+        img: img after crop.
+        lab: lab after crop.
+    """
+    # Random pad and crop
+    crop_h = crop_size[0]
+    crop_w = crop_size[1]
+    lab = tf.cast(lab, dtype=tf.float32)
+    lab = lab - 255.
+    combined = tf.concat(axis=2, values=[img, lab]) 
+    img_shape = tf.shape(img)
+    combined_pad = tf.image.pad_to_bounding_box(combined, 0, 0, tf.maximum(crop_h, img_shape[0]), tf.maximum(crop_w, img_shape[1]))
+    last_img_dim = tf.shape(img)[-1]
+    last_lab_dim = last_img_dim + tf.shape(lab)[-1]
+    combined_crop = tf.random_crop(combined_pad, [crop_h, crop_w, last_lab_dim])
+    img = combined_crop[:, :, : last_img_dim]
+    lab = combined_crop[:, :, last_img_dim :]
+    lab = lab + 255.
     lab = tf.cast(lab, dtype=tf.uint8)
     return img, lab
 
@@ -201,7 +255,7 @@ def read_data_list(data_list):
     return imgs, labs
 
 
-def read_images_from_disk(input_queue, shuffle, random_color, random_mirror, random_rotate, img_mean):
+def read_images_from_disk(input_queue, is_training, img_mean):
     """
     Read image, and label with optional pre-processing.
     """
@@ -214,48 +268,24 @@ def read_images_from_disk(input_queue, shuffle, random_color, random_mirror, ran
     # RGB -> BGR
     img_r, img_g, img_b = tf.split(axis=2, num_or_size_splits=3, value=img)
     img = tf.cast(tf.concat(axis=2, values=[img_b, img_g, img_r]), dtype=tf.float32)
-    # Expand in dimension 0 for resize
-    img = tf.expand_dims(img, dim=0)
-    lab = tf.expand_dims(lab, dim=0)
-    # Train
-    if shuffle:
-        # Randomly rescale the image, normal and valid mask
-        scale = tf.random_uniform([1], minval=0.5, maxval=1.5, dtype=tf.float32)
-        h_new = tf.to_int32(tf.multiply(tf.to_float(image_size[0]), scale))
-        w_new = tf.to_int32(tf.multiply(tf.to_float(image_size[1]), scale))
-        new_shape = tf.squeeze(tf.stack([h_new, w_new]), squeeze_dims=[1])
-        img = tf.image.resize_bilinear(img, new_shape)
-        lab = tf.image.resize_nearest_neighbor(lab, new_shape)
-    # Remove dimension 0
-    img = tf.squeeze(img, axis=0)
-    lab = tf.squeeze(lab, axis=0)
-    # Train
-    if shuffle:
-        # Random pad and crop
-        crop_h = crop_size[0]
-        crop_w = crop_size[1]
-        lab = tf.cast(lab, dtype=tf.float32)
-        img = img - 255.
-        lab = lab - 255.
-        combined = tf.concat(axis=2, values=[img, lab]) 
-        img_shape = tf.shape(img)
-        combined_pad = tf.image.pad_to_bounding_box(combined, 0, 0, tf.maximum(crop_h, img_shape[0]), tf.maximum(crop_w, img_shape[1]))
-        last_img_dim = tf.shape(img)[-1]
-        last_lab_dim = last_img_dim + tf.shape(lab)[-1]
-        combined_crop = tf.random_crop(combined_pad, [crop_h, crop_w, last_lab_dim])
-        img = combined_crop[:, :, : last_img_dim]
-        lab = combined_crop[:, :, last_img_dim :]
-        img = img + 255.
-        lab = lab + 255.
-        lab = tf.cast(lab, dtype=tf.uint8)
-    if random_mirror:
-        img, lab = image_mirror(img, lab)
-    if random_rotate:
-        img, lab = image_rotate(img, lab)
-    if random_color:
+
+    if is_training:
+        # Random color
         img = distort_color(img, np.random.randint(24))
-    # Extract mean.
-    img -= img_mean
+        # Extract mean.
+        img -= img_mean
+        # Random mirror
+        img, lab = image_mirror(img, lab)
+        # Random scale
+        img, lab = image_scale(img, lab)
+        # Random crop
+        img, lab = image_crop(img, lab)
+        # Random_rotate:
+        img, lab = image_rotate(img, lab)
+    else:
+        # Extract mean.
+        img -= img_mean
+
     # Set static shape so that tensorflow knows shape at compile time.
     img.set_shape((480, 640, 3))
     lab.set_shape((480, 640, 1))
@@ -268,7 +298,7 @@ class Reader(object):
     from the disk, and enqueues them into a TensorFlow queue.
     """
 
-    def __init__(self, coord, data_list, shuffle=False, random_color=False, random_mirror=False, random_rotate=False, img_mean=img_mean):
+    def __init__(self, coord, data_list, is_training=True, img_mean=img_mean):
         """
         Initialize a Reader.
         """
@@ -277,8 +307,8 @@ class Reader(object):
         self.img_list, self.lab_list = read_data_list(data_list)
         self.imgs = tf.convert_to_tensor(self.img_list, dtype=tf.string)
         self.labs = tf.convert_to_tensor(self.lab_list, dtype=tf.string)
-        self.queue = tf.train.slice_input_producer([self.imgs, self.labs], shuffle=shuffle)
-        self.img, self.lab = read_images_from_disk(self.queue, shuffle, random_color, random_mirror, random_rotate, img_mean)
+        self.queue = tf.train.slice_input_producer([self.imgs, self.labs], shuffle=is_training)
+        self.img, self.lab = read_images_from_disk(self.queue, is_training, img_mean)
 
     def dequeue(self, num_elements):
         img_bat, lab_bat = tf.train.batch([self.img, self.lab], num_elements)
